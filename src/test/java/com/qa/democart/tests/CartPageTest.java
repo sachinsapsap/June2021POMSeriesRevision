d04a83602e9c921a8815dc0cf8875607439acf82
sachinsapsap <sachinsapsap@gmail.com>
2024-03-21 09:52:36 +0530

    Cart page changes

---
+++ src/test/java/com/qa/democart/tests/CartPageTest.java
@@ -0,0 +1,13 @@
+package com.qa.democart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartPageTest {
+	
+	@Test
+	public void testCart() {
+		Assert.assertEquals(true, true);
+	}
+
+}
